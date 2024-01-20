f87a8f31d094a51092a74fbbf235c98dd5703215
Helwa <hhalloum879@gmail.com>
2024-01-19 22:39:27 -0800

    first commit

---
+++ src/scenes/StatsOverlay.js
@@ -0,0 +1,14 @@
+class StatsOverlay extends Phaser.Scene {
+    constructor() {
+        super('statsoverlayScene')
+        console.log("StatsOverlay: constructor")
+    }
+
+    init() {
+        console.log('StatsOverLay: init')
+    }
+
+    update() {
+        //console.log('StatsOverlay: update')
+    }
+}
